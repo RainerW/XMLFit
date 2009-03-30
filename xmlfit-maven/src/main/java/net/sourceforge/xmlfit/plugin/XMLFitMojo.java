51af8ece3f145c6e9f0bf996ed9a4ae2b266c6a5
rainerw <rainerw@d61e4ca7-080a-40f3-933c-416ff52d7d0a>
2009-03-30 14:03:42 +0000

    Support for multilple Testsuits added. (Supports now testsuites Element)
    git-svn-id: https://xmlfit.svn.sourceforge.net/svnroot/xmlfit/trunk@164 d61e4ca7-080a-40f3-933c-416ff52d7d0a

--- xmlfit-maven/src/main/java/net/sourceforge/xmlfit/plugin/XMLFitMojo.java
+++ xmlfit-maven/src/main/java/net/sourceforge/xmlfit/plugin/XMLFitMojo.java
@@ -6,126 +6,135 @@ import org.apache.maven.plugin.AbstractMojo;
 
 import net.sourceforge.xmlfit.runner.XMLFit;
 import net.sourceforge.xmlfit.runner.XMLFitCommandLineRunner;
-import net.sourceforge.xmlfit.runner.XMLFitRunner;
-
 
 /**
  * Transform XML testfiles to HTML.
  * @goal transform
  */
-public class XMLFitMojo extends AbstractMojo implements XMLFitRunner
+public class XMLFitMojo extends AbstractMojo
 {
   /**
-   * The path to the testsuite.
-   *
-   * @parameter testsuite
+   * The path to the testsuites.
+   * 
+   * @parameter
    */
-  private String testsuite;
-  
+  private String[] testsuites;
+
   /**
    * The input folder.
-   *
+   * 
    * @parameter inputDir
    */
   private String inputDir;
-  
+
   /**
    * The output folder.
-   *
+   * 
    * @parameter outputDir
    */
   private String outputDir;
-  
+
   /**
    * The custom CSS File.
-   *
+   * 
    * @parameter stylesheet
    */
   private String stylesheet;
-  
+
   /**
    * The custom CSS File.
-   *
+   * 
    * @parameter mode
    */
   private String mode;
-  
+
   private static final int DELAY = 6000;
-  private static Logger logger = Logger.getLogger(XMLFitCommandLineRunner.class);
-  
+
+  private static Logger logger = Logger
+      .getLogger(XMLFitCommandLineRunner.class);
+
   public void execute()
   {
     PropertyConfigurator.configureAndWatch("log4j.properties", DELAY);
     try
     {
-     logger.info("Start XMLFit");
-     XMLFit.run(this);
-     logger.info("End XMLFit");
+      logger.info("Start XMLFit");
+      XMLFitMojoRunner runner = new XMLFitMojoRunner();
+      runner.setInputDir(getInputDir());
+      runner.setMode(getMode());
+      runner.setOutputDir(getOutputDir());
+      runner.setStylesheet(getStylesheet());
+      for (String suite : getTestsuite())
+      {
+        logger.info("XMLFit running next suite : " + suite);
+        runner.setTestsuite(suite);
+        XMLFit.run(runner);
+      }
+      logger.info("End XMLFit");
     }
     catch (Exception e)
     {
       e.printStackTrace();
     }
-   
+
   }
+
   public String getStylesheet()
   {
-    if(!(stylesheet == null))
+    if (!(stylesheet == null))
     {
       return stylesheet;
     }
     return null;
   }
-  
+
   public String getMode()
   {
-    if(!(mode == null))
+    if (!(mode == null))
     {
       return mode;
     }
     return null;
   }
-  
+
   public void setMode(String mode)
   {
     this.mode = mode;
   }
-  
+
   public void setStylesheet(String stylesheet)
   {
     this.stylesheet = stylesheet;
   }
-  
-  public String getTestsuite()
+
+  public String[] getTestsuite()
   {
-    return testsuite;
+    return this.testsuites;
   }
-  
-  public void setTestsuite(String testsuite)
+
+  public void setTestsuites(String[] testsuite)
   {
-    this.testsuite = testsuite;
+    this.testsuites = testsuite;
   }
 
   public String getInputDir()
   {
     return inputDir;
   }
-  
+
   public void setInputDir(String inputDir)
   {
     this.inputDir = inputDir;
   }
 
-
   public String getOutputDir()
   {
     return outputDir;
   }
-  
+
   public void setOutputDir(String outputDir)
   {
     this.outputDir = outputDir;
   }
-  
-  
+
 }
---
+++ xmlfit-maven/src/main/java/net/sourceforge/xmlfit/plugin/XMLFitMojoRunner.java
@@ -0,0 +1,71 @@
+package net.sourceforge.xmlfit.plugin;
+
+import net.sourceforge.xmlfit.runner.XMLFitRunner;
+
+/**
+ * Parameter Object to run multiple XMLFit tests
+ */
+public class XMLFitMojoRunner implements XMLFitRunner
+{
+
+  private String inputDir;
+
+  private String mode;
+
+  private String outputDir;
+
+  private String stylesheet;
+
+  private String testSuite;
+
+  public String getInputDir()
+  {
+    return inputDir;
+  }
+
+  public String getMode()
+  {
+    return mode;
+  }
+
+  public String getOutputDir()
+  {
+    return outputDir;
+  }
+
+  public String getStylesheet()
+  {
+    return stylesheet;
+  }
+
+  public String getTestsuite()
+  {
+    return testSuite;
+  }
+
+  public void setTestsuite(String testsuite)
+  {
+    testSuite = testsuite;
+  }
+
+  public void setInputDir(String inputDir)
+  {
+    this.inputDir = inputDir;
+  }
+
+  public void setMode(String mode)
+  {
+    this.mode = mode;
+  }
+
+  public void setOutputDir(String outputDir)
+  {
+    this.outputDir = outputDir;
+  }
+
+  public void setStylesheet(String stylesheet)
+  {
+    this.stylesheet = stylesheet;
+  }
+
+}
